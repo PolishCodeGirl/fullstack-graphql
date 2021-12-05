73ff405b0902080c95afaacffafd097f8cb4041c
Marzena Malachowska <malachowska.marzena@gmail.com>
2021-12-05 12:04:03 +0100

    Add new pet using mutation

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -16,20 +16,36 @@ const ALL_PETS = gql`
   }
 `;
 
+const ADD_NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 export default function Pets() {
   const [modal, setModal] = useState(false);
   const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPetResponse] = useMutation(ADD_NEW_PET);
 
   const onSubmit = (input) => {
     setModal(false);
+    createPet({
+      variables: { newPet: input },
+    });
   };
 
-  if (loading) {
+  if (loading || newPetResponse.loading) {
     return <Loader />;
   }
 
-  if (error) {
-    return <p>{error.message}</p>;
+  if (error || newPetResponse.error) {
+    if (error) return <p>{error.message}</p>;
+    if (newPetResponse.error) return <p>{newPetResponse.error.message}</p>;
   }
 
   if (modal) {
